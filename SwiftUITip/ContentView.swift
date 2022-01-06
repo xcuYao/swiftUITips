df1bd245b2c124af1413f1b9b448d4fcd521ccfb
yaoning <chinayaoning@gmail.com>
2022-01-06 15:45:26 +0800

    Initial Commit

---
+++ SwiftUITip/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITip
+//
+//  Created by yaoning on 2022/1/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
